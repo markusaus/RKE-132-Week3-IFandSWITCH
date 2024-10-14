26dd9c51e8c20e92805911b43ee5b43f3522fd0d
Markus <markus.aus@tktk.ee>
2024-10-14 12:04:34 +0300

    add week3 code

---
+++ Week3_Test3_Question10/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Mis hinde teie saite?");
+string userInput = Console.ReadLine();
+char userGrade;
+
+if (char.TryParse(userInput, out userGrade))
+switch (userGrade)
+{
+    case 'A':
+        Console.WriteLine("Suurepärane!");
+        break;
+    case 'B':
+        Console.WriteLine("Väga hea!");
+        break;
+    case 'C':
+        Console.WriteLine("Hea!");
+        break;
+    case 'D':
+        Console.WriteLine("Rahuldav");
+        break;
+    case 'E':
+        Console.WriteLine("Kasin");
+        break;
+    case 'F':
+        Console.WriteLine("Puudulik");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus");
+        break;
+}
+
---
+++ Week3_Test3_Question9/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.WriteLine("Mis hinde teie saite?");
+string userInput =Console.ReadLine();
+char userGrade;
+if(char.TryParse(userInput,out userGrade))
+
+
+if (userGrade == 'A')
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (userGrade == 'B')
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (userGrade == 'C')
+{
+    Console.WriteLine("Hea!");
+}
+else if (userGrade == 'D')
+{
+    Console.WriteLine("Rahuldav");
+}
+else if (userGrade == 'E')
+{
+    Console.WriteLine("Kasin");
+}
+else if (userGrade == 'F')
+{
+    Console.WriteLine("Puudulik");
+}
+else
+{
+    Console.WriteLine("vale väärtus");
+}
+
